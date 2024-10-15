fb859ea3530328b9d55468261e9be9d2f33a15d2
ddewaele <ddewaele@gmail.com>
2024-10-15 22:15:10 +0200

    Initial commit

---
+++ test/cdk-cognito-hell.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCognitoHell from '../lib/cdk-cognito-hell-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-cognito-hell-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCognitoHell.CdkCognitoHellStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
